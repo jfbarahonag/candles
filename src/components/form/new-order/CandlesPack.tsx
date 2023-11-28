6fd51be4c9c0040723e187bda3adce7432960540
jfbarahonag <jfbarahonag@unal.edu.co>
2023-11-27 22:15:43 -0500

    trying to set the names but it failed

--- src/components/form/new-order/CandlesPack.tsx
+++ src/components/form/new-order/CandlesPack.tsx
@@ -1,8 +1,9 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 import CandlesPackNames from "./CandlesPackNames";
 import CandlesPackOptions from "./CandlesPackOptions";
 import { ICandlesPack } from "./interfaces";
+import { useNewFormContext } from "./NewOrderContext";
 
 const getColorButton = (disabled: boolean): string => {
   return `${
@@ -13,17 +14,64 @@ const getColorButton = (disabled: boolean): string => {
 };
 
 const getTextButton = (disabled: boolean): string => {
-  return `${
-    disabled
-      ? "Editar"
-      : "Guardar"
-  }`;
+  return `${disabled ? "Editar" : "Guardar"}`;
 };
 
 const CandlesPack = ({ candlesPack }: { candlesPack: ICandlesPack }) => {
+  const { candlesPacks, getCandlesPackById } = useNewFormContext();
+
+  const [candlesPacksCopy, setCandlesPacksCopy] = useState(structuredClone(candlesPacks));
+
   const [disableCandlesPack, setDisableCandlesPack] = useState(false);
 
+  const updateName = (id: string, idx: number, value: string) => {
+    const cp = structuredClone(
+      candlesPacksCopy.find(cp => cp.id === id)
+    );
+    if (!cp) return;
+    console.log("valid cp")
+    cp.names[idx] = value;
+
+    setCandlesPacksCopy((prev) =>
+      prev.map(
+        (cpc) => (cpc.id === cp.id ? structuredClone(cp) : structuredClone(cpc))
+      )
+    );
+    console.log("first")
+  }
+
+  useEffect(() => {
+    console.log(candlesPacksCopy)
+  }, [candlesPacksCopy])
+
+  useEffect(() => {
+    setCandlesPacksCopy( prevCpc => {
+        if (candlesPacks.length === prevCpc.length + 1) {
+          // new item
+          return structuredClone([candlesPacks[0], ...prevCpc])
+        } else if (candlesPacks.length < prevCpc.length) {
+          // remove item
+          return structuredClone(
+            prevCpc.filter(obj1 => candlesPacks.find(obj2 => obj2.id === obj1.id))
+          );
+        }
+        // update item
+        return candlesPacks.map(cp => cp);
+      }
+    );
+  }, [candlesPacks])
+  
+
   const saveCandlesPack = () => {
+    if (!disableCandlesPack) {
+      //is going to save
+      const currentCandlesPack = getCandlesPackById(candlesPack.id);
+      const a = document.getElementById(`${currentCandlesPack?.id}-${1}`)?.id
+      console.log(a)
+      alert(JSON.stringify(currentCandlesPack));
+
+    }
+
     setDisableCandlesPack(!disableCandlesPack);
   };
 
@@ -31,7 +79,7 @@ const CandlesPack = ({ candlesPack }: { candlesPack: ICandlesPack }) => {
     <>
       <fieldset className="flex flex-col w-full" disabled={disableCandlesPack}>
         <CandlesPackOptions candlePackId={candlesPack.id} />
-        <CandlesPackNames candlePackId={candlesPack.id} />
+        <CandlesPackNames candlePackId={candlesPack.id} updateName={updateName} />
       </fieldset>
       {candlesPack.type !== "Desconocido" && (
         <div className="my-3 px-3 w-full flex flex-row justify-center">
--- src/components/form/new-order/CandlesPackNames.tsx
+++ src/components/form/new-order/CandlesPackNames.tsx
@@ -1,32 +1,68 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useNewFormContext } from "./NewOrderContext";
 import CandlesPackNameFields from "./CandlesPackNameFields";
 
-const CandlesPackNames = ({ candlePackId }: { candlePackId: string }) => {
-  const { getCandlesPackById } = useNewFormContext();
-  
+const CandlesPackNames = ({
+  candlePackId,
+  updateName,
+}: {
+  candlePackId: string;
+  updateName: (id: string, idx: number, value: string) => void;
+}) => {
+  const { candlesPacks, getCandlesPackById } = useNewFormContext();
+
+  const [localNames, setlocalNames] = useState(structuredClone(
+    getCandlesPackById(candlePackId)?.names || [])
+  );
   const [currentCandlesPack, setCurrentCandlesPack] = useState(
     getCandlesPackById(candlePackId)
   );
 
-  if (!currentCandlesPack) {
-    return <h1>NOT FOUND</h1>
-  }
+  const changeNameOfCandlesPack = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const [id, idxStr] = e.target.id.split(" - ");
+    const idx = Number(idxStr);
+    if (isNaN(idx)) return;
+    updateName(id, idx, e.target.value);
+    setlocalNames(prev => [
+      ...prev.slice(0, idx),
+      e.target.value,
+      ...prev.slice(idx + 1),
+    ]);
+  };
+
+  useEffect(() => {
+    console.log("se actualizo candles packs")
+    setCurrentCandlesPack(
+      getCandlesPackById(candlePackId)
+    );
+  }, [candlesPacks])
   
+
+  if (!currentCandlesPack) {
+    return <h1>NOT FOUND</h1>;
+  }
+
   return (
     <ul className="px-3 w-full">
-      {
-        (currentCandlesPack.type === 'Colores' || currentCandlesPack.type === 'Doradas') &&
+      {(currentCandlesPack.type === "Colores" ||
+        currentCandlesPack.type === "Doradas") &&
         currentCandlesPack.names.map((name, idx) => (
-          <CandlesPackNameFields 
-            key={`${idx}-${name}`} 
-            candlesPack={currentCandlesPack} 
-            fieldIdx={idx} 
-          />
-        ))
-      }
+          <div className="flex" key={`${name}-${idx}`}>
+            <div className="w-1/12 flex flex-col justify-center">
+              <span>{idx + 1}.</span>
+            </div>
+            <input
+              value={localNames[idx]}
+              id={`${currentCandlesPack.id} - ${idx}`}
+              className="w-10/12 my-2 p-1"
+              type="text"
+              placeholder="Nombre de la vela"
+              onChange={changeNameOfCandlesPack}
+            />
+          </div>
+        ))}
     </ul>
-  )
-}
+  );
+};
 
-export default CandlesPackNames
+export default CandlesPackNames;
